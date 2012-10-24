952b770ccd548903766c1ebd527d0870a3b9cd95
Ricardo Barron-Silva <ricardob@thephotosync.com>
2012-10-24 19:39:36 +0000

    initial calculator

---
+++ guestbook.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# 
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import cgi
+import datetime
+import webapp2
+
+from google.appengine.ext import db
+from google.appengine.api import users
+
+class Greeting(db.Model):
+  author = db.UserProperty()
+  content = db.StringProperty(multiline=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write('<html><body>')
+
+#    greetings = db.GqlQuery("SELECT * "
+#                            "FROM Greeting "
+#                            "ORDER BY date DESC LIMIT 10")
+#
+#    for greeting in greetings:
+#      if greeting.author:
+#        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+#      else:
+#        self.response.out.write('An anonymous person wrote:')
+#      self.response.out.write('<blockquote>%s</blockquote>' %
+#                              cgi.escape(greeting.content))
+#
+
+    self.response.out.write("""
+          <form action="/add" method="post">
+            <div><input name ="firstNum" type="text"/></div>
+            <div><input name="secondNum" type="text"/></div>
+            <div><input name="addButton" type="submit" value="add values"></div>
+            <div><input name="subtractButton" type="submit" value="subtract  values"></div>
+          </form>
+        </body>
+      </html>""")
+
+
+class Adder(webapp2.RequestHandler):
+  def post(self):
+    # greeting = Greeting()
+    # if users.get_current_user():
+    #   greeting.author = users.get_current_user()
+
+    # greeting.content = self.request.get('content')
+    # greeting.put()
+    # self.redirect('/')
+    addButtonClicked = self.request.get('addButton')
+    try:
+      firstNum = int (self.request.get('firstNum'))
+      secondNum = int (self.request.get('secondNum'))
+    except ValueError:
+      self.response.out.write('Please use only numbers!!')
+    if addButtonClicked:
+      self.response.out.write(firstNum+secondNum)
+    else:
+      self.response.out.write(firstNum-secondNum)
+# Cool story bro.
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage),
+  ('/add', Adder)
+], debug=True)
